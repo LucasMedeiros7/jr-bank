19430b8e49b97f70284e3a5265765e2d5d37a9b5
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-10-17 21:42:14 -0300

    teste unitário ListAccountsUseCase

---
+++ src/domain/usecases/ListAccountsUseCase.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryAccountRepository } from '../../infra/repositories/InMemoryAccountRepository';
+import { CreateAccountUseCase } from './CreateAccountUseCase';
+import { ListAccountsUseCase } from './ListAccountsUseCase';
+
+describe('Get balance by id use case', () => {
+  it('should list all accounts registered', async () => {
+    const accountRepository = new InMemoryAccountRepository();
+    const listAccountsUseCase = new ListAccountsUseCase(accountRepository);
+    const createAccountUseCase = new CreateAccountUseCase(accountRepository);
+
+    await createAccountUseCase.execute({
+      name: 'Lucas',
+      cpf: '123.998.890-70',
+      password: 'sistema123teste'
+    });
+
+    await createAccountUseCase.execute({
+      name: 'John',
+      cpf: '188.847.190-54',
+      password: 'fakepassword'
+    });
+
+    const allAccounts = await listAccountsUseCase.execute();
+
+    expect(allAccounts).toHaveLength(2);
+    expect(allAccounts).toMatchObject([
+      {
+        name: 'Lucas',
+        cpf: '123.998.890-70'
+      },
+      {
+        name: 'John',
+        cpf: '188.847.190-54'
+      }
+    ]);
+  });
+});
