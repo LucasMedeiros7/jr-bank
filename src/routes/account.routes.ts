9deaa7188a6748ccd9954cebe9e380ba764007f3
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-10-19 14:51:10 -0300

    fix: importação do AccountController no arquivo de rotas

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { AccountController } from '../controllers/CreateAccountController';
+import { AccountController } from '../controllers/AccountController';
 
 const accountRoutes = Router();
 
