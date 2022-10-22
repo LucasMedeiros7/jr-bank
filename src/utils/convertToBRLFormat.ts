96aaef98a18e44bfede0207c7af7ae942d176b9d
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-10-22 11:17:10 -0300

    Conversor de centavos para formato BRL

---
+++ src/utils/convertToBRLFormat.ts
@@ -0,0 +1,10 @@
+export function convertToBRLFormat(integer: number) {
+  let integertoDecimal = integer / 100;
+
+  const formatted = integertoDecimal.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  });
+
+  return 'R$ ' + formatted.replace('R$', '').trim();
+}
